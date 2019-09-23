9da0b82363d9deb5749ffec1e6fe18cb4f848d36
Angelo Garofalo <angelo.garofalo@unibo.it>
2019-09-23 16:00:31 +0200

    add headers

--- SYNTH/clean.csh
+++
@@ -1,3 +0,0 @@
-rm -rf *.log.* *.log *.svf export/* mapped/* reports/* work unmapped/* ulpcluster_LIB *.spf
-rm scan_insertion/*
-rm synth_log.txt 
--- SYNTH/rm_setup/suppress_messages.tcl
+++
@@ -1,2 +0,0 @@
-suppress_message {VO-12 VO-2  UID-51 PSYN-025  TIM-164   ELAB-311  VER-735   VER-318  VER-61  ELAB-982  }
-suppress_message {VER-130 }
