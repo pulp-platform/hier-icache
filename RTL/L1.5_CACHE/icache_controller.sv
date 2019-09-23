dc8d984c93998f0c9eca789258e9ead44a0b8bcb
Angelo Garofalo <angelo.garofalo@unibo.it>
2019-09-23 15:59:19 +0200

    add headers

--- RTL/L1.5_CACHE/icache_controller.sv
+++ RTL/L1.5_CACHE/icache_controller.sv
@@ -1,3 +1,14 @@
+// Copyright 2019 ETH Zurich and University of Bologna.
+// Copyright and related rights are licensed under the Solderpad Hardware
+// License, Version 0.51 (the "License"); you may not use this file except in
+// compliance with the License. You may obtain a copy of the License at
+// http://solderpad.org/licenses/SHL-0.51. Unless required by applicable law
+// or agreed to in writing, software, hardware and materials distributed under
+// this License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the
+// specific language governing permissions and limitations under the License.
+
+
 `define log2_size(VALUE)    ((VALUE) <= ( 1 ) ? 1 : (VALUE) < ( 2 ) ? 1 : (VALUE) < ( 4 ) ? 2 : (VALUE)< (8) ? 3:(VALUE) < ( 16 )  ? 4 : 5 )
 `include "ulpsoc_defines.sv"
 
