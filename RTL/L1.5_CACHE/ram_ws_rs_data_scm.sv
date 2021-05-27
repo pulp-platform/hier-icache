5578e4986fe10d35bab4afa61a132a0555b94505
Alfio Di Mauro <adimauro@iis.ee.ethz.ch>
2021-05-27 18:33:27 +0200

    add support for latch-based/ff-based implementation instantiate the tc_sram module for the SRAM-based implementation

--- RTL/L1.5_CACHE/ram_ws_rs_data_scm.sv
+++ RTL/L1.5_CACHE/ram_ws_rs_data_scm.sv
@@ -8,9 +8,7 @@
 // CONDITIONS OF ANY KIND, either express or implied. See the License for the
 // specific language governing permissions and limitations under the License.
 
-
 `include "pulp_soc_defines.sv"
-// `define  USE_SRAM
 
 module ram_ws_rs_data_scm
 #(
@@ -29,123 +27,38 @@ module ram_ws_rs_data_scm
     output logic [data_width-1:0]       rdata
 );
 
-`ifdef USE_DATA_SRAM
+`ifdef HIER_ICACHE_USE_DATA_SRAM
 
-    logic cs_n;
-    logic we_n;
+    tc_sram #(.NumWords(2**addr_width), .DataWidth(data_width), .NumPorts(1)) i_sram_data (
+        .clk_i  (clk    ),
+        .rst_ni (rst_n  ),
+        .req_i  (req    ),
+        .we_i   (write  ),
+        .addr_i (addr   ),
+        .wdata_i(wdata  ),
+        .be_i   ('1     ),
+        .rdata_o(rdata  )
+    );
 
-    assign  cs_n = ~req;
-    assign  we_n = ~write;
-
-    generate
-      if(data_width==128)
-      begin : SRAM_CUT
-
-        case (addr_width)
-            5: begin
-                logic [4-1:0]   n_aw;
-                logic [1-1:0]   n_ac;
-                logic [127:0]   bw;
-                assign {n_aw, n_ac} = addr;
-                assign bw = (we_n) ?  '0 : '1;
-
-                // GF22
-                SPREG_32w_128b sram_data
-                (
-
-                    .CLK      ( clk   ), // input
-                    .CEN      ( cs_n  ), // input
-                    .RDWEN    ( we_n  ), // input
-                    .AW       ( n_aw  ), // input [3:0]
-                    .AC       ( n_ac  ), // input
-                    .D        ( wdata ), // input [127:0]
-                    .BW       ( '1    ), // input [127:0]
-                    .T_LOGIC  ( 1'b0  ), // input
-                    .MA_SAWL  ( '0    ), // input
-                    .MA_WL    ( '0    ), // input
-                    .MA_WRAS  ( '0    ), // input
-                    .MA_WRASD ( '0    ), // input
-                    .Q        ( rdata ), // output [127:0]
-                    .OBSV_CTL (       )  // output
-                );
-
-                // TSMC55
-                // SRAM_SP_32w_128b sram_data
-                // (
-                //     .CS_N    ( cs_n  ),
-                //     .CLK     ( clk   ),
-                //     .WR_N    ( we_n  ),
-                //     .RW_ADDR ( addr  ),
-                //     .RST_N   ( rst_n ),
-                //     .DATA_IN ( wdata ),
-                //     .DATA_OUT( rdata )
-                // );
-            end
-
-            6: begin
-                logic          n_as;
-                logic [2:0]    n_aw;
-                logic [1:0]    n_ac;
-                logic [127:0]   bw;
-                assign {n_aw[2], n_as, n_aw[1:0], n_ac} = addr;
-                assign bw = (we_n) ?  '0 : '1;
-
-                SP1D_64w_128b sram_data
-                (
-                    .CLK         ( clk           ),
-                    .CEN         ( cs_n          ),
-                    .RDWEN       ( we_n          ),
-                    .DEEPSLEEP   ( 1'b0          ),
-                    .POWERGATE   ( 1'b0          ),
-                    .AS          ( n_as          ),
-                    .AW          ( n_aw          ),
-                    .AC          ( n_ac          ),
-                    .D           ( wdata         ),
-                    .BW          ( bw            ),
-                    .T_BIST      ( 1'b0          ),
-                    .T_LOGIC     ( 1'b0          ),
-                    .T_CEN       ( 1'b0          ),
-                    .T_RDWEN     ( 1'b0          ),
-                    .T_DEEPSLEEP ( 1'b0          ),
-                    .T_POWERGATE ( 1'b0          ),
-                    .T_STAB      ( '0            ),
-                    .T_WBT       ( '0            ),
-                    .T_AS        ( '0            ),
-                    .T_AW        ( '0            ),
-                    .T_AC        ( '0            ),
-                    .T_D         ( '0            ),
-                    .T_BW        ( '0            ),
-                    .MA_SAWL     ( '0            ),
-                    .MA_WL       ( '0            ),
-                    .MA_WRAS     ( '0            ),
-                    .MA_WRASD    ( '0            ),
-                    .Q           ( rdata         ),
-                    .OBSV_CTL    (               )
-                );
-            end
-            default : /* default */;
-        endcase
-
-      end
-
-    endgenerate
 `else
 
  `ifdef PULP_FPGA_EMUL
     register_file_1r_1w
  `else
-    register_file_1r_1w_test_wrap
+    `ifdef HIER_ICACHE_DATA_MEM_USE_LATCHES
+      latch_register_file_1r_1w_test_wrap
+    `else
+      ff_register_file_1r_1w_test_wrap
+    `endif 
  `endif
     #(
         .ADDR_WIDTH(addr_width),
         .DATA_WIDTH(data_width)
     )
-    scm_data
+    i_scm_data
     (
         .clk         ( clk          ),
-    `ifdef PULP_FPGA_EMUL
         .rst_n       ( rst_n        ),
-    `endif
 
         // Read port
         .ReadEnable  ( req & ~write ),
--- RTL/L1.5_CACHE/ram_ws_rs_tag_scm.sv
+++ RTL/L1.5_CACHE/ram_ws_rs_tag_scm.sv
@@ -10,7 +10,6 @@
 
 
 `include "pulp_soc_defines.sv"
-// `define  USE_SRAM
 
 
 module ram_ws_rs_tag_scm
@@ -28,76 +27,37 @@ module ram_ws_rs_tag_scm
     output logic [data_width-1:0]    rdata
 );
 
-`ifdef USE_TAG_SRAM
-    logic cs_n;
-    logic we_n;
+`ifdef HIER_ICACHE_USE_TAG_SRAM
 
-    assign  cs_n = ~req;
-    assign  we_n = ~write;
-
-    generate
-      if(data_width==10 && addr_width==5)
-      begin : SRAM_CUT
-
-        logic [2:0]     n_aw;
-        logic [1:0]     n_ac;
-        logic [9:0]     bw;
-        assign {n_aw, n_ac} = addr;
-        assign bw = (we_n) ? '0 : '1;
-
-        // GF22
-        SPREG_32w_10b sram_tag
-        (
-
-            .CLK      ( clk   ), // input
-            .CEN      ( cs_n  ), // input
-            .RDWEN    ( we_n  ), // input
-            .AW       ( n_aw  ), // input [3:0]
-            .AC       ( n_ac  ), // input
-            .D        ( wdata ), // input [9:0]
-            .BW       ( '1    ), // input [9:0]
-            .T_LOGIC  ( 1'b0  ), // input
-            .MA_SAWL  ( '0    ), // input
-            .MA_WL    ( '0    ), // input
-            .MA_WRAS  ( '0    ), // input
-            .MA_WRASD ( '0    ), // input
-            .Q        ( rdata ), // output [9:0]
-            .OBSV_CTL (       )  // output
-        );
-/*
-          SRAM_SP_32w_10b sram_tag
-          (
-            .CS_N    ( cs_n  ),
-            .CLK     ( clk   ),
-            .WR_N    ( we_n  ),
-            .RW_ADDR ( addr  ),
-            .RST_N   ( rst_n ),
-            .DATA_IN ( wdata ),
-            .DATA_OUT( rdata )
-          );
-*/
-      end
-
-
-    endgenerate
+    tc_sram #(.NumWords(2**addr_width), .DataWidth(data_width), .NumPorts(1)) i_sram_tag (
+        .clk_i  (clk    ),
+        .rst_ni (rst_n  ),
+        .req_i  (req    ),
+        .we_i   (write  ),
+        .addr_i (addr   ),
+        .wdata_i(wdata  ),
+        .be_i   ('1     ),
+        .rdata_o(rdata  )
+    );
 
 `else
    `ifdef PULP_FPGA_EMUL
       register_file_1r_1w
    `else
-      register_file_1r_1w_test_wrap
+      `ifdef HIER_ICACHE_TAG_MEM_USE_LATCHES
+        latch_register_file_1r_1w_test_wrap
+      `else
+        ff_register_file_1r_1w_test_wrap
+      `endif 
    `endif
       #(
         .ADDR_WIDTH(addr_width),
         .DATA_WIDTH(data_width)
       )
-      scm_tag
+      i_scm_tag
       (
-        .clk           (clk),
-  `ifdef PULP_FPGA_EMUL
-        .rst_n         (rst_n),
-  `endif
-
+        .clk         ( clk          ),
+        .rst_n       ( rst_n        ),
         // Read port
         .ReadEnable  ( req & ~write ),
         .ReadAddr    ( addr         ),
